fd3aa31a3112fcc06f87b6054051b70fbd3497cb
Alexander Sosedkin <monk@unboiled.info>
2022-01-17 21:17:07 +0100

    flake.nix: define hydraJobs

--- flake.nix
+++ flake.nix
@@ -3,11 +3,19 @@
 
   inputs.bootstrap-from-tcc.url = "github:ZilchOS/bootstrap-from-tcc";
 
-  outputs = { self, bootstrap-from-tcc }: {
-    packages.x86_64-linux = (import pkgs/default.nix) {
-      bootstrap-musl = bootstrap-from-tcc.packages.x86_64-linux.libc;
-      bootstrap-toolchain = bootstrap-from-tcc.packages.x86_64-linux.toolchain;
-      bootstrap-busybox = bootstrap-from-tcc.packages.x86_64-linux.busybox;
-    };
-  };
+  outputs = { self, bootstrap-from-tcc }:
+    let
+      bootstrapPkgs = bootstrap-from-tcc.packages.x86_64-linux;
+      inputPkgs = {
+        bootstrap-musl = bootstrapPkgs.libc;
+        bootstrap-toolchain = bootstrapPkgs.toolchain;
+        bootstrap-busybox = bootstrapPkgs.busybox;
+      };
+      corePkgs = (import pkgs/default.nix) inputPkgs;
+    in
+      {
+        packages.x86_64-linux = corePkgs;
+
+        hydraJobs = builtins.mapAttrs (_: drv: {x86_64-linux = drv;}) corePkgs;
+      };
 }
