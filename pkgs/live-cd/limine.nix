4a08cfd4db4c2772a12002f1c0c8ba1440d1080a
Alexander Sosedkin <monk@unboiled.info>
2022-03-04 02:04:29 +0100

    pkgs/live-cd/limine: update to 2.86 with serial support

--- pkgs/live-cd/limine.nix
+++ pkgs/live-cd/limine.nix
@@ -1,16 +1,16 @@
 { name ? "limine", stdenv, fetchurl, gnumake, nasm, gnubinutils, gnumtools }:
 
-#> FETCH 256091d39bcce88de5cf9fd15c3be50f4270e9b78ed81916104b5573f0645406
-#>  FROM https://github.com/limine-bootloader/limine/releases/download/v2.84.2/limine-2.84.2.tar.xz
+#> FETCH 271451ff3659960f2f326b8b63e7d9eaa97e425a8e215cddf03ed02a78c68f22
+#>  FROM https://github.com/limine-bootloader/limine/releases/download/v2.86/limine-2.86.tar.xz
 
 stdenv.mkDerivation {
   pname = name;
-  version = "2.84.2";
+  version = "2.86";
 
   src = fetchurl {
-    # local = /downloads/limine-2.84.2.tar.gz;
-    url = "https://github.com/limine-bootloader/limine/releases/download/v2.84.2/limine-2.84.2.tar.xz";
-    sha256 = "256091d39bcce88de5cf9fd15c3be50f4270e9b78ed81916104b5573f0645406";
+    # local = /downloads/limine-2.86.tar.xz;
+    url = "https://github.com/limine-bootloader/limine/releases/download/v2.86/limine-2.86.tar.xz";
+    sha256 = "271451ff3659960f2f326b8b63e7d9eaa97e425a8e215cddf03ed02a78c68f22";
   };
 
   buildInputs = [ gnumake nasm gnubinutils gnumtools ];
