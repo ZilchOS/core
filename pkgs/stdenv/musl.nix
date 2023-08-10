f7aaf2bb1bd5f3bc17c13ed5208bb63eee240539
Alexander Sosedkin <monk@unboiled.info>
2023-08-11 00:12:32 +0200

    pkgs/stdenv/musl: update to 1.2.4

--- pkgs/stdenv/musl.nix
+++ pkgs/stdenv/musl.nix
@@ -2,12 +2,12 @@
 
 stdenv.mkDerivation {
   inherit pname;
-  version = "1.2.2";
+  version = "1.2.4";
 
   src = fetchurl {
-    # local = /downloads/musl-1.2.2.tar.gz;
-    url = "http://musl.libc.org/releases/musl-1.2.2.tar.gz";
-    sha256 = "9b969322012d796dc23dda27a35866034fa67d8fb67e0e2c45c913c3d43219dd";
+    # local = /downloads/musl-1.2.4.tar.gz;
+    url = "http://musl.libc.org/releases/musl-1.2.4.tar.gz";
+    sha256 = "7a35eae33d5372a7c0da1188de798726f68825513b7ae3ebe97aaaa52114f039";
   };
 
   buildInputs = [ stdenv.clang stdenv.busybox gnumake ];
