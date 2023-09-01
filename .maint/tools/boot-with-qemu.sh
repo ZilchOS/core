1ed1acbfa5aaba2557a440adf1dfda8b88786801
Alexander Sosedkin <monk@unboiled.info>
2023-09-01 12:15:21 +0200

    .maint/tools/boot-with-qemu: fix attribute access

--- .maint/tools/boot-with-qemu.sh
+++ .maint/tools/boot-with-qemu.sh
@@ -10,7 +10,7 @@ mkdir -p .tmp
 if [[ "$USE_CCACHE" == 1 ]]; then
   ./.maint/tools/build-using-ccache.sh -o.tmp/live-cd-$$ live-cd.iso
 else
-  nix build --option warn-dirty false -o.tmp/live-cd-$$ '.#live-cd.iso'
+  nix build --option warn-dirty false -o.tmp/live-cd-$$ '.#live-cd^iso'
 fi
 
 qemu-system-x86_64 --accel kvm -m 512 -cdrom .tmp/live-cd-$$-iso "$@"
