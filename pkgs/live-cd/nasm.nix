7a6eaa63d13f73534785d377e3c7f6c671a1ad91
Alexander Sosedkin <monk@unboiled.info>
2023-09-03 16:49:17 +0200

    pkgs/live-cd/nasm: update to 2.16.01

--- pkgs/live-cd/nasm.nix
+++ pkgs/live-cd/nasm.nix
@@ -1,16 +1,16 @@
 { name ? "nasm", stdenv, fetchurl, gnumake }:
 
-#> FETCH 3caf6729c1073bf96629b57cee31eeb54f4f8129b01902c73428836550b30a3f
-#>  FROM https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/nasm-2.15.05.tar.xz
+#> FETCH c77745f4802375efeee2ec5c0ad6b7f037ea9c87c92b149a9637ff099f162558
+#>  FROM https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/nasm-2.16.01.tar.xz
 
 stdenv.mkDerivation {
   pname = name;
-  version = "2.15.05";
+  version = "2.16.01";
 
   src = fetchurl {
-    # local = /downloads/nasm-2.15.05.tar.xz;
-    url = "https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/nasm-2.15.05.tar.xz";
-    sha256 = "3caf6729c1073bf96629b57cee31eeb54f4f8129b01902c73428836550b30a3f";
+    # local = /downloads/nasm-2.16.01.tar.xz;
+    url = "https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/nasm-2.16.01.tar.xz";
+    sha256 = "c77745f4802375efeee2ec5c0ad6b7f037ea9c87c92b149a9637ff099f162558";
   };
 
   buildInputs = [ gnumake ];
