c154d0fdce44833a1547beb7f9e2845bd82dd9c5
Alexander Sosedkin <monk@unboiled.info>
2023-09-03 16:44:15 +0200

    pkgs/live-cd/gnuxorriso: update to 1.5.6.pl02

--- pkgs/live-cd/gnuxorriso.nix
+++ pkgs/live-cd/gnuxorriso.nix
@@ -1,16 +1,16 @@
 { name ? "gnuxorriso", stdenv, fetchurl, gnumake, linux-headers }:
 
-#> FETCH 0wi92lxpm3dcjglmmfbh4z37w8jmbx0qmhh98gvzbjwx98ykkiry
-#>  FROM https://www.gnu.org/software/xorriso/xorriso-1.5.4.pl02.tar.gz
+#> FETCH 786f9f5df9865cc5b0c1fecee3d2c0f5e04cab8c9a859bd1c9c7ccd4964fdae1
+#>  FROM https://www.gnu.org/software/xorriso/xorriso-1.5.6.pl02.tar.gz
 
 stdenv.mkDerivation {
   pname = name;
-  version = "1.5.4";
+  version = "1.5.6";
 
   src = fetchurl {
-    # local = /downloads/xorriso-1.5.4.pl02.tar.gz;
-    url = "https://www.gnu.org/software/xorriso/xorriso-1.5.4.pl02.tar.gz";
-    sha256 = "0wi92lxpm3dcjglmmfbh4z37w8jmbx0qmhh98gvzbjwx98ykkiry";
+    # local = /downloads/xorriso-1.5.6.pl02.tar.gz;
+    url = "https://www.gnu.org/software/xorriso/xorriso-1.5.6.pl02.tar.gz";
+    sha256 = "786f9f5df9865cc5b0c1fecee3d2c0f5e04cab8c9a859bd1c9c7ccd4964fdae1";
   };
 
   buildInputs = [ gnumake ];
