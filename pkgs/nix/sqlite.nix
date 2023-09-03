9719de94fe90f66c6fdd0fc11c7d8cc42b7948de
Alexander Sosedkin <monk@unboiled.info>
2023-09-03 15:34:16 +0200

    pkgs/nix/sqlite: update to 3430000

--- pkgs/nix/sqlite.nix
+++ pkgs/nix/sqlite.nix
@@ -1,16 +1,16 @@
 { name ? "sqlite", stdenv, fetchurl, gnumake }:
 
-#> FETCH bd90c3eb96bee996206b83be7065c9ce19aef38c3f4fb53073ada0d0b69bbce3
-#>  FROM https://www.sqlite.org/2021/sqlite-autoconf-3360000.tar.gz
+#> FETCH 49008dbf3afc04d4edc8ecfc34e4ead196973034293c997adad2f63f01762ae1
+#>  FROM https://sqlite.org/2023/sqlite-autoconf-3430000.tar.gz
 
 stdenv.mkDerivation {
   pname = name;
-  version = "3360000";
+  version = "3430000";
 
   src = fetchurl {
-    # local = /downloads/sqlite-autoconf-3360000.tar.gz;
-    url = "https://www.sqlite.org/2021/sqlite-autoconf-3360000.tar.gz";
-    sha256 = "bd90c3eb96bee996206b83be7065c9ce19aef38c3f4fb53073ada0d0b69bbce3";
+    # local = /downloads/sqlite-autoconf-3430000.tar.gz;
+    url = "https://www.sqlite.org/2023/sqlite-autoconf-3430000.tar.gz";
+    sha256 = "49008dbf3afc04d4edc8ecfc34e4ead196973034293c997adad2f63f01762ae1";
   };
 
   buildInputs = [ gnumake ];
