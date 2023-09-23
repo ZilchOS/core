700a6cc0da5243ce43f0330648cb146a09a9da93
Alexander Sosedkin <monk@unboiled.info>
2023-09-23 03:55:33 +0200

    .maint/tools/hashes: port to busybox ash

--- .maint/tools/hashes.sh
+++ .maint/tools/hashes.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 : ${UPDATE=0}
 : ${USE_CCACHE=0}
@@ -6,17 +6,17 @@
 
 set -ue
 
-if [[ "$SERIAL" != 1 ]]; then
+if [ "$SERIAL" != 1 ]; then
     # first build everything from .maint/hashes in parallel
     targets=''
     while IFS=" " read -r _ pkg; do
-	if [[ "$USE_CCACHE" == 1 ]]; then
-	    targets+=" $pkg"
+	if [ "$USE_CCACHE" == 1 ]; then
+	    targets="$targets $pkg"
 	else
-	    targets+=" .#$pkg"
+	    targets="$targets .#$pkg"
 	fi
     done < .maint/hashes
-    if [[ "$USE_CCACHE" == 1 ]]; then
+    if [ "$USE_CCACHE" == 1 ]; then
 	./.maint/tools/build-using-ccache.sh --no-link $targets "$@"
     else
 	nix build --option warn-dirty false --no-link $targets "$@"
@@ -24,25 +24,29 @@ if [[ "$SERIAL" != 1 ]]; then
 fi
 
 # then print the hash differences and update the hashfile
-if [[ "$UPDATE" == 1 ]]; then mkdir -p .tmp; :> .tmp/out-$$; fi
+RET=0
+if [ "$UPDATE" == 1 ]; then mkdir -p .tmp; :> .tmp/out-$$; fi
 while IFS=" " read -r old_hash pkg; do
-    if [[ "$USE_CCACHE" == 1 ]]; then
+    if [ "$USE_CCACHE" == 1 ]; then
 	./.maint/tools/build-using-ccache.sh -o.tmp/res-$$ "$pkg" "$@"
     else
 	nix build ".#$pkg" --option warn-dirty false -o .tmp/res-$$ "$@"
     fi
-    new_hash=$(basename $(readlink .tmp/res-$$*))
+    new_path=$(readlink .tmp/res-$$*)
+    new_hash=$(echo $new_path | sed -E 's|.*/([a-z0-9]{32})-.*|\1|')
     rm .tmp/res-$$*
-    [[ $new_hash =~ ^([a-z0-9]{32})-.* ]]
-    new_hash=${BASH_REMATCH[1]}
-    if [[ "$old_hash" == "$new_hash" ]]; then
-	    echo "  $old_hash $pkg"
+    if [ "$new_hash" == "$old_hash" ]; then
+        echo "  $old_hash $pkg"
     else
-	    echo "- $old_hash $pkg"
-	    echo "+ $new_hash $pkg"
+        echo "- $old_hash $pkg"
+        echo "+ $new_hash $pkg"
+        RET=1
     fi
-    if [[ "$UPDATE" == 1 ]]; then echo "$new_hash $pkg" >> .tmp/out-$$; fi
+    if [ "$UPDATE" == 1 ]; then echo "$new_hash $pkg" >> .tmp/out-$$; fi
 done < .maint/hashes
-# TODO: possibly with ccache
 
-if [[ "$UPDATE" == 1 ]]; then mv .tmp/out-$$ .maint/hashes; fi
+if [ "$UPDATE" == 1 ]; then
+    mv .tmp/out-$$ .maint/hashes;
+else
+    exit $RET
+fi
